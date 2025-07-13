c4a6be79637bbef137cb3352383b8f1f120dd094
Martin <schalleberg@gmail.com>
2025-07-13 17:58:52 +0200

    comment display stuff

--- Arduino/battery_motitor_WLAN/battery_motitor_WLAN.ino
+++ Arduino/battery_motitor_WLAN/battery_motitor_WLAN.ino
@@ -18,12 +18,13 @@ typedef struct {
 
 
 
-#include <Adafruit_GFX.h>
+/*#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     1 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+*/
 
 
 //#define RUN_ON_PC
