9d81f6369c9a9fe7b527475cef4b9db78b1dcc42
Martin Muehlemann <martin.muehlemann@swisshof.ch>
2025-07-21 21:31:19 +0200

    post battery voltage in volkszähler

--- Heizung/batterymonitor.py
+++ Heizung/batterymonitor.py
@@ -7,8 +7,8 @@ import databases
 
 
 if __name__ == '__main__':
-    #host = "192.168.2.1"
-    host = "172.19.12.50"
+    host = "192.168.2.1"
+    #host = "172.19.12.50"
     
     port = 80  # socket server port number
 
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     deserialzedData = json.loads(resp.text)
     print(deserialzedData)
     
-    databases.sendToFirebase("battery/batteryVoltage", deserialzedData["voltage"],"Batteriespannung")
-    
+    #databases.sendToFirebase("battery/batteryVoltage", deserialzedData["voltage"],"Batteriespannung")
+    databases.sendToVolkszaehler("localhost", "0f8549e0-6669-11f0-98b0-cf5b56a3e9e5", deserialzedData["voltage"], "Batteriespannung")
     
